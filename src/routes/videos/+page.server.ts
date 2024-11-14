edec7df2db4119c4d6e7783f1e6e3cbcf8d34800
Conner Donohoo <connerdonohoo@Conner-Donohoo-OneView.local>
2024-11-14 14:29:35 -0700

    Fix reserved env variable

--- src/routes/videos/+page.server.ts
+++ src/routes/videos/+page.server.ts
@@ -1,4 +1,4 @@
-import { AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION } from "$env/static/private";
+import { AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET_REGION } from "$env/static/private";
 import {
 	S3Client,
 	ListObjectsV2Command,
@@ -6,7 +6,7 @@ import {
 } from "@aws-sdk/client-s3";
 
 const client = new S3Client({
-	region: AWS_REGION,
+	region: AWS_BUCKET_REGION,
 	credentials: {
 		accessKeyId: AWS_ACCESS_KEY,
 		secretAccessKey: AWS_SECRET_KEY,
